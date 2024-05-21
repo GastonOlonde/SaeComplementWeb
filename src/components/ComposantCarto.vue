1b6f042b13b6d684d31d8dc8155aaef052318105
gastonolonde <gastonolonde@gmail.com>
2024-05-21 19:03:31 +0200

    carto position uptade

--- src/components/ComposantCarto.vue
+++ src/components/ComposantCarto.vue
@@ -1,25 +1,64 @@
 <template>
   <h1>Cartographie</h1>
   <div id="map"></div>
+  <button @click="centerMap">Centrer sur moi</button>
 </template>
 
 <script>
+import { ref, onMounted } from 'vue';
 import L from 'leaflet';
 
 export default {
-  name: 'CartographieView',
-  mounted() {
-    const map = L.map('map').setView([48.8534, 2.3488], 12);
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '© OpenStreetMap contributors'}).addTo(map);
-    
-    L.marker([49.871144, 2.2641492]).addTo(map)
-    .bindPopup('L\'iut magueule')
-    .openPopup();
+  setup() {
+    const map = ref(null);
+
+    onMounted(() => {
+      map.value = L.map('map').setView([48.8534, 2.3488], 12);
+      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '© OpenStreetMap contributors'
+      }).addTo(map.value);
+
+      L.marker([49.871144, 2.2641492]).addTo(map.value)
+        .bindPopup('L\'iut magueule')
+        .openPopup();
+    });
+
+    const centerMap = () => {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition((position) => {
+          const { latitude, longitude } = position.coords;
+          map.value.eachLayer((layer) => {
+            if (layer instanceof L.Marker) {
+              map.value.removeLayer(layer);
+            }
+          });
+          map.value.setView([latitude, longitude], 12);
+          L.marker([latitude, longitude])
+            .addTo(map.value)
+            .bindPopup('Vous êtes ici')
+            .openPopup();
+
+        }, (error) => {
+          console.error(error);
+        });
+      } else {
+        console.error('Geolocation is not supported by this browser.');
+      }
+    };
+
+    return {
+      centerMap
+    };
   }
-}
+};
+
+
 </script>
 
 <style scoped>
-#map { height: 80vh; }
+#map { 
+  height: 80vh;
+  width: 80%;
+  margin: 0 auto;
+}
 </style>
--- src/views/CartographieView.vue
+++ src/views/CartographieView.vue
@@ -1,9 +1,11 @@
 <template>
   <div>
     <ComposantCarto />
+    <ComposantRecupPosition />
   </div>
 </template>
 
 <script setup>
 import ComposantCarto from '@/components/ComposantCarto.vue'
+import ComposantRecupPosition from '@/components/ComposantRecupPosition.vue';
 </script>
