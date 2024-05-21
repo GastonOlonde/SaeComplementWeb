e5474d227b0c6469a92d16c5d69c7d7f866da797
gastonolonde <gastonolonde@gmail.com>
2024-05-21 16:45:34 +0200

    récupération des données

--- src/components/ComposantGeo.vue
+++ src/components/ComposantGeo.vue
@@ -1,18 +1,41 @@
-
+<script>
+  import axios from 'axios'
+  const data = ref([]);
+  axios.get('http://localhost:1337/api/saes')
+    .then(response => {
+      data.value = response.data
+      console.log(response.data)
+    })
+    .catch(error => {
+      console.error(error)
+    })
+  
+</script>
 <template>
   <p>
     Compteur : {{ count }}
   </p>
   <button 
-    @click="handleClick">Incrémenter
+    @click="incremente">Incrémenter
   </button>
+  <button
+    @click="decremente">Décrémenter
+  </button>
+  <button
+    @click="count = 0">Réinitialiser
+  </button>
+  <p>
+    {{ data }}
+  </p>
 </template>
 
 <script setup>
 import { ref } from 'vue'
 const count = ref(0);
-
-function handleClick() {
+function incremente() {
   count.value++;
+}
+function decremente() {
+  count.value--;
 }
 </script>
